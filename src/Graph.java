1c0fbb5f39327b3ffcb4af98f1ad538a698aa334
Artur Sołtys <arturac2@gmail.com>
2020-12-16 18:34:59 +0100

    add simulated annealing with 3 coefficients

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Graph {
 	private int[][] neighborhoodMatrix;
 	protected int vertexAmount;
-	
+
 	// Konstruktor Klasy Graph
 	public Graph(int vertexAmount) {
 		this.vertexAmount = vertexAmount;
@@ -10,16 +13,16 @@ public class Graph {
 
 		for (int i = 0; i < vertexAmount; i++) {
 			for (int j = 0; j < vertexAmount; j++) {
-					this.neighborhoodMatrix[i][j] = 0;
+				this.neighborhoodMatrix[i][j] = 0;
 			}
 		}
 	}
 
 	// Metoda tworz¹ca krawêdz pomiêdzy danymi wierzcho³kami o podanej wadze
 	public boolean addEdge(int v, int w, int weight) {
-		//if (weight >= 1000)
-			// Waga krawedzi musi byc mniejsza od 1000
-		//	weight = 999;
+		// if (weight >= 1000)
+		// Waga krawedzi musi byc mniejsza od 1000
+		// weight = 999;
 
 		if (this.neighborhoodMatrix[v][w] > 0)
 			return false;
@@ -54,6 +57,157 @@ public class Graph {
 
 	}
 
+	public ArrayList<Integer> greedyAlgorithm() {
+		Random random = new Random();
+		int startVertex = random.nextInt(this.vertexAmount);
+		ArrayList<Integer> tour = new ArrayList<Integer>();
+		tour.add(startVertex);
+		
+		// Rozpatrywanie wierzcho³ka z ktorego chcemy wykonac ruch
+		for (int i = 0; i < this.vertexAmount - 1; i++) {
+			int minEdgeCost = -1;
+			int nextVertex = -1;
+
+			// Rozpatrywanie wierzcho³ka do ktorego chcemy przejsc
+			for (int j = 0; j < this.vertexAmount; j++) {
+				// Jeœli kolejny wierzcho³ek jest startowym
+				// lub droga od niego jest w³aœnie rozpatrywana to odrzucamy
+
+				if (tour.get(tour.size() - 1) == j || tour.get(0) == j)
+					continue;
+
+				// Odrzucenie krawedzi do wierzcholka umieszczonego juz na trasie
+				boolean vertexUsed = false;
+				for (int k = 0; k < tour.size(); k++) {
+					if (j == tour.get(k)) {
+						vertexUsed = true;
+						break;
+					}
+				}
+				if (vertexUsed)
+					continue;
+
+				// Znalezienie najkrotszej mozliwej jeszcze do uzycia krawedzi
+				int consideredEdgeCost = this.getWeight(tour.get(tour.size() - 1), j);
+
+				if (minEdgeCost == -1 || minEdgeCost > consideredEdgeCost) {
+					minEdgeCost = consideredEdgeCost;
+					nextVertex = j;
+				}
+			}
+
+			tour.add(nextVertex);
+		}
+
+		//tour.add(startVertex);
+		return tour;
+
+	}
+
+	// Algorytm Symulowanego Wy¿arzania z s¹siedztwem typu swap
+	public void simulatedAnnealing(int maxTime, float[] alpha) {
+		
+		Random random = new Random();
+		double endTime, execTime;
+		ArrayList<Integer> startTour = greedyAlgorithm();
+		displayTour(startTour);
+		ArrayList<Integer> currentTour;
+		ArrayList<Integer> minTour;
+		for (int i = 0; i < alpha.length; i++)
+		{
+			System.out.println("#"+ i);
+			double startTime = System.nanoTime();
+			currentTour = new ArrayList<Integer>(startTour);
+			minTour = new ArrayList<Integer>(startTour);
+			int minCost = getTourCost(minTour);
+			int currentCost = getTourCost(currentTour);
+			double temperature = ((double) getTourCost(startTour) * 3);
+			System.out.println("Wspolczynnik zmiany temperatury wynosi: " + alpha[i]);
+			System.out.println("Temperatura pocz¹tkowa wynosi: " + temperature);
+			do {
+				for (int j = 0; j < this.vertexAmount; j++) 
+				{
+					ArrayList<Integer> swappedTour = swapVertexes(currentTour);
+					int nextTourCost = getTourCost(swappedTour);
+
+					if (nextTourCost < currentCost) 
+					{
+						currentTour = new ArrayList<Integer>(swappedTour);
+						currentCost = nextTourCost;
+						
+						if(nextTourCost < minCost) 
+						{
+							minCost = nextTourCost;
+							minTour = new ArrayList<Integer>(swappedTour);
+						}
+						
+					} 
+					else if (random.nextFloat() < Math.exp(-((nextTourCost - currentCost) / temperature))) 
+					{
+						currentTour = new ArrayList<Integer>(swappedTour);
+						currentCost = nextTourCost;
+					}
+				}
+				temperature *= alpha[i];
+				endTime = System.nanoTime();
+				execTime = (endTime - startTime) / 1000000000;
+			} while (execTime <= maxTime);
+			System.out.println("Temperatura koncowa wynosi: " + temperature);
+			System.out.println("Czas wykonywania wynosi: " + execTime + " s");
+			System.out.println("Trasa: " );
+			displayTour(minTour);
+		}
+
+	}
+
+	// Metoda zamieniaj¹ca wierzcho³ki w trasie (tworzy nastêpnie rozpatrywane
+	// rozwi¹zanie)
+	private ArrayList<Integer> swapVertexes(ArrayList<Integer> tour) {
+		ArrayList<Integer> swappedTour = new ArrayList<Integer>(tour);
+
+		int v, w;
+		Random random = new Random();
+
+		do {
+			v = random.nextInt(tour.size());
+			w = random.nextInt(tour.size());
+
+		} while (v == w);
+		Collections.swap(swappedTour, v, w);
+		
+		return swappedTour;
+	}
+
+	// Wyswietlenie trasy wraz z jej kosztem
+	public void displayTour(ArrayList<Integer> tour) {
+
+		int distFromStart = 0;
+		int length = 0;
+		for (int i = 0; i < tour.size() - 1; i++) {
+			length = this.getWeight(tour.get(i), tour.get(i+1));
+			distFromStart += length;
+
+			System.out.print(tour.get(i));
+			System.out.print(" -> ");
+		}
+		distFromStart += this.getWeight(tour.get(tour.size()-1), tour.get(0));
+		System.out.print(tour.get(0));
+		System.out.println();
+		System.out.println("Dlugosc trasy: " + distFromStart);
+	}
+
+	// Metoda obliczaj¹ca koszt trasy
+	public int getTourCost(ArrayList<Integer> tour) {
+		int tourCost = 0;
+		int length = 0;
+		for (int i = 0; i < tour.size() - 1; i++) {
+			length = this.getWeight(tour.get(i), tour.get(i+1));
+			tourCost += length;
+		}
+		tourCost += this.getWeight(tour.get(tour.size() -1 ), tour.get(0));
+		return tourCost;
+	}
+
 	public int[][] getNeighborhoodMatrix() {
 		return neighborhoodMatrix;
 	}
@@ -61,7 +215,7 @@ public class Graph {
 	public void setNeighborhoodMatrix(int[][] neighborhoodMatrix) {
 		this.neighborhoodMatrix = neighborhoodMatrix;
 	}
-	
+
 	public int getVertexAmount() {
 		return vertexAmount;
 	}
@@ -69,7 +223,5 @@ public class Graph {
 	public void setVertexAmount(int vertexAmount) {
 		this.vertexAmount = vertexAmount;
 	}
-	
-	
-	
+
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,63 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
 
 	@SuppressWarnings("resource")
+	public static Graph readGraphFromFile() throws FileNotFoundException {
+
+		Scanner sc = new Scanner(System.in);
+		File file = null;
+		int selected = sc.nextInt();
+		switch (selected) {
+		case 1:
+			file = new File("ftv47.atsp");
+			break;
+		case 2:
+			file = new File("ftv170.atsp");
+			break;
+		case 3:
+			file = new File("rbg403.atsp");
+			break;
+		default:
+			break;
+		}
+		Scanner scanner = new Scanner(file);
+
+		for (int i = 0; i < 3; i++) {
+			if (scanner.hasNextLine()) {
+				scanner.nextLine();
+			}
+		}
+
+		if (scanner.hasNext()) {
+			scanner.next();
+		}
+
+		int dimension = scanner.nextInt();
+		System.out.println(dimension);
+		for (int i = 0; i < 4; i++) {
+			if (scanner.hasNextLine()) {
+				scanner.nextLine();
+			}
+		}
+
+		Graph graph = new Graph(dimension);
+		for (int i = 0; i < dimension; i++) {
+			for (int j = 0; j < dimension; j++) {
+				graph.addEdge(i, j, scanner.nextInt());
+			}
+		}
+		return graph;
+	}
+
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
+		float[] alpha = {0.999f, 0.9999f, 0.99999f}; // wspolczynnik zmiany temperatury do SW
+		int maxTime = 0; // Kryterium stopu
 		int selection;
 		Graph graph = null;
 		do {
@@ -14,7 +65,7 @@ public class Menu {
 			System.out.println("2. Wprowadzenie kryterium stopu");
 			System.out.println("3. Wybór s¹siedztwa");
 			System.out.println("4. Uruchomienie algorytmu TS dla wczytanych danych i ustawieñ");
-			System.out.println("5. Ustawienie wspólczynnika zmiany temperatury dla SW");
+			System.out.println("5. Ustawienie wspólczynnikow zmiany temperatury dla SW");
 			System.out.println("6. Uruchomianie algorytmu SW dla wczytanych danych i ustawieñ");
 			System.out.println("7. Pomiary");
 			System.out.println("Aby zakonczyc - 0");
@@ -22,162 +73,48 @@ public class Menu {
 			Scanner sc = new Scanner(System.in);
 			selection = sc.nextInt();
 			switch (selection) {
-			
+
 			// Wczytanie danych z pliku
 			case 1: {
-				System.out.println("Wprowadz nazwê pliku: ");
-				sc = new Scanner(System.in);
-				File file = new File("ALL_atsp\\" + sc.nextLine());
-				System.out.println(file.getName());
-				Scanner scanner = new Scanner(file);
-				
-				
-				for (int i = 0; i < 3; i++) {
-					if(scanner.hasNextLine()) 
-					{
-						scanner.nextLine();
-					}
-				}
-				
-				if (scanner.hasNext()) 
-				{
-					 scanner.next();
-				}
-				
-				int dimension = scanner.nextInt();
-				System.out.println(dimension);
-				for (int i = 0; i < 4; i++) 
-				{
-					if (scanner.hasNextLine())
-					{
-						scanner.nextLine();
-					}
-				}
-				
-				graph = new Graph(dimension);
-				for (int i = 0; i < dimension; i++) {
-					for (int j = 0; j < dimension; j++) {
-						graph.addEdge(i, j, scanner.nextInt());
-					}
-				}
-				graph.displayGraph();
+				System.out.println("Wybierz plik, z ktorego wczytaæ dane: ");
+				System.out.println("1. ftv47.atsp");
+				System.out.println("2. ftv170.atsp");
+				System.out.println("3. rbg403.atsp");
+				graph = readGraphFromFile();
+				//graph.displayGraph();
 
 			}
 				break;
-/*
+
 			case 2: {
-				int vertex;
-				System.out.println("Liczba miast: ");
-				vertex = sc.nextInt();
 
-				graph = new Graph(vertex);
+				System.out.println("Wprowad¿ czas jako kryterium stopu w sekundach: ");
+				maxTime = sc.nextInt();
 
-				Graph.generateRandomFullGraph(graph, maxSalesmanDistance);
 			}
 				break;
-			case 3: {
-				if (graph != null)
-					graph.displayGraph();
-				else
-					System.out.println("Brak danych do wyœwietlenia");
-			}
-				break;
-			case 4: {
-				if (graph != null) {
-					// pomiar czasu w ms
-					long nanosActualTime = System.nanoTime();
-					ArrayList<Integer> route = Graph.ATSPBruteForce(graph);
-					long executionTime = System.nanoTime() - nanosActualTime;
 
-					// Wyswietlenie trasy
-					int distFromStart = 0;
-					int length = 0;
-					System.out.print(route.get(0) + " -> ");
-					for (int i = 1; i < route.size(); i++) {
-						length = graph.getWeight(route.get(i - 1), route.get(i));
-						distFromStart += length;
-
-						System.out.print(route.get(i));
-						if(i!= route.size()-1) {
-							System.out.print(" -> ");
-						}
-					}
-					System.out.println();
-					System.out.println("Dlugosc trasy: " + distFromStart);
-
-					System.out.println("Czas wykonania algorytmu [ms]: " + executionTime/1000000);
-				} else
-					System.out.println(" Brak zaladowanych danych ");
-			}
-				break;
+			/*
+			 * case 3: {
+			 *  } 
+			 *  break;
+			 */
 			case 5: {
-				if (graph != null) {
-					long nanosActualTime = System.nanoTime();
-					ArrayList<Integer> route = Graph.ATSPBranchAndBound(graph);
-					long executionTime = System.nanoTime() - nanosActualTime;
-
-					// Wyswietlenie trasy
-					int distFromStart = 0;
-					int length = 0;
-					System.out.print(route.get(0) + " -> ");
-					for (int i = 1; i < route.size(); i++) {
-						length = graph.getWeight(route.get(i - 1), route.get(i));
-						distFromStart += length;
-
-						System.out.print(route.get(i));
-						if(i!= route.size()-1) {
-							System.out.print(" -> ");
-						}
-					}
-					System.out.println();
-					System.out.println("Dlugosc trasy: " + distFromStart);
-
-					System.out.println("Czas wykonania algorytmu [ms]: " + executionTime/1000000);
-				} else
-					System.out.println(" Brak zaladowanych danych ");
+				System.out.println("Wprowadz wspolczyniki zmiany temperatury < 1: ");
+				
+					alpha[0] = sc.nextFloat();
+					alpha[1] = sc.nextFloat();
+					alpha[2] = sc.nextFloat();
 			}
 				break;
-				
-			case 6: {
-				if(graph != null) {
-					long nanosActualTime = System.nanoTime();
-					int minCost = Graph.ATSPDynamicProgramming(graph);
-					long executionTime = System.nanoTime() - nanosActualTime;
-					
-					System.out.println("Dlugosc trasy: " + minCost);
 
-					System.out.println("Czas wykonania algorytmu [ms]: " + executionTime/1000000);
+			case 6: {
+				if (graph != null) {
+					graph.simulatedAnnealing(maxTime, alpha);
 				}
 			}
 				break;
-			
-			case 7: {
 
-		            int average;
-		            int max_iteration = 100; // iloœæ pomiarów dla danego N
-		            System.out.println();
-		            System.out.println("----- BB -----");
-		            for (int nIterator = 9; nIterator < 15; nIterator++) {
-		                int sum = 0;
-		                for (int actualIterator = 0; actualIterator < max_iteration; actualIterator++) {
-		                	Graph graph2 = new Graph(nIterator);
-		                    Graph.generateRandomFullGraph(graph2, maxSalesmanDistance);
-		                    long nanosActualTime = System.nanoTime();
-		                    Graph.ATSPBranchAndBound(graph2);
-		                    long executionTime = (System.nanoTime() - nanosActualTime)/1000000;
-		                    sum += executionTime;
-		                    System.out.print(" \r" + (100 / max_iteration)* (actualIterator + 1) + "%");
-		                }
-		                System.out.println();
-		                average = sum / max_iteration;
-		                System.out.println("N = " + nIterator + ", sredni czas = " + average + "[ms]" );
-	            }
-		            
-				
-			}
-			break;
-			*/
-			
 			case 0: {
 			}
 				break;
@@ -186,7 +123,6 @@ public class Menu {
 			}
 			}
 		} while (selection != 0);
-
 
 	}
 
